c6b62306911a44715377f2805555f818c3ef8b6b
scart <myfakeusernow+fuck+off@gmail.com>
2020-05-18 22:21:06 +0200

    Simple database setup tool

---
+++ src/SetupTemplateDatabase/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using DbUp;
+
+namespace SetupTemplateDatabase
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var connectionString =
+                args.FirstOrDefault()
+                ?? "Server=.; Database=poco-template; Integrated Security=False; User Id=sa;Password=SpikePrototype2016!;";
+
+            // Clean database
+            DropDatabase.For.SqlDatabase(connectionString);
+            EnsureDatabase.For.SqlDatabase(connectionString);
+            
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+            
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif                
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+        }
+    }
+}
---
+++ src/SetupTemplateDatabase/Scripts/0001 - hello_world.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[HelloWorld]
+(
+    [Id]      INT IDENTITY (1, 1),
+    [Message] NVARCHAR(32) NOT NULL,
+    
+    PRIMARY KEY ([Id])
+);
